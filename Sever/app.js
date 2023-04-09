dce2bc03cab0050996f546221dda1abdaf02ce84
Sidit Srivastava <siditsrivastava84@gmail.com>
2023-04-09 19:58:52 +0530

    FOOD_ORDERING_WEBSITE

---
+++ Sever/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Fooddata = require("./Modal/schema");
+const dotenv = require('dotenv')
+const cookieParser = require("cookie-parser")
+
+dotenv.config({
+  path : "./config.env"
+})
+
+app.use(cookieParser());
+
+const DATABASE = process.env.DATABASE
+
+const DB =
+      DATABASE
+  mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB, {})
+  .then(() => {
+    console.log("Correction Successful");
+  })
+  .catch((err) => {
+    console.log("no Correction");
+  });
+
+  app.use(express.json())
+
+  app.use(require("./auths"));
+
+
+app.listen(5000 , () => {
+  console.log("sidit")
+})
